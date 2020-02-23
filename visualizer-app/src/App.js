a8b1e429eba57b8554105cd8423f103a68a52217
vj-krishnan <40969053+vj-krishnan@users.noreply.github.com>
2020-02-23 22:30:37 +0530

    Add files via upload

---
+++ visualizer-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SortingVisualizer from './SortingVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualizer></SortingVisualizer>
+    </div>
+  );
+}
+
+export default App;
